9f69177ef2049cbaf1d70c1b77a8eaeb123a7fc8
Munoz <152181111+Kvrmea@users.noreply.github.com>
2024-10-11 22:27:24 +0200

    cube

---
+++ script.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(70, iw/ih)
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+const material = new THREE.MeshPhongMaterial({color: 0xffffff})
+
+const mesh = new THREE.Mesh( geometry, material)
+
+const ligth = new THREE.PointLight(0xeeeeee)
+
+scene.add(mesh)
+scene.add(ligth)
+
+camera.position.set(0, 0, 2)
+ligth.position.set(0, 0, 2)
+
+const renderer = new THREE.WebGLRenderer({canvas})
+
+loop()
+
+function loop() {
+    requestAnimationFrame(loop)
+    mesh.rotation.y += 0.01
+    mesh.rotation.x += 0.005
+    renderer.render(scene, camera)
+}
+renderer.render(scene, camera)
